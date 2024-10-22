61c9a3a83329724a7bd2452c8e89b9abdab180f5
Justin Sam <jusitnsam02544@gmail.com>
2024-10-22 14:46:12 -0400

    Completed ToDo 3

--- app/src/main/java/edu/farmingdale/pizzapartybottomnavbar/Screen3.kt
+++ app/src/main/java/edu/farmingdale/pizzapartybottomnavbar/Screen3.kt
@@ -32,14 +32,20 @@ import androidx.compose.ui.unit.sp
 
 @Composable
 fun Screen3() {
+    val clrStops = arrayOf(
+        0.0f to Color.Black,
+        0.05f to Color.LightGray,
+        1.0f to Color.White
+    )
+
     var sliderValue by remember { mutableStateOf(0.5f) }
     var chkd by remember { mutableStateOf(true) }
 
 
     val context = LocalContext.current
-    Column ( modifier = Modifier.padding(horizontal = 20.dp).fillMaxSize(),
+    Column ( modifier = Modifier.padding(horizontal = 20.dp).fillMaxSize().background(Brush.verticalGradient(colorStops = clrStops)),
         verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally){
+        horizontalAlignment = Alignment.CenterHorizontally,){
         Slider(value = sliderValue, onValueChange = { sliderValue=it }, Modifier.fillMaxWidth()
             , enabled = chkd)
 
