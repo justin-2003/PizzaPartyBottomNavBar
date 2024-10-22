2997f2f58c2de06e0159b95be22ce028753fff09
Justin Sam <jusitnsam02544@gmail.com>
2024-10-22 14:36:09 -0400

    Completed ToDo 1

--- app/src/main/java/edu/farmingdale/pizzapartybottomnavbar/ui/SplashScreen.kt
+++ app/src/main/java/edu/farmingdale/pizzapartybottomnavbar/ui/SplashScreen.kt
@@ -1,15 +1,13 @@
-package edu.farmingdale.pizzapartybottomnavbar
+package edu.farmingdale.pizzapartybottomnavbar.ui
 
 import android.view.animation.OvershootInterpolator
 import androidx.compose.animation.core.Animatable
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
@@ -17,6 +15,8 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.navigation.NavHostController
+import edu.farmingdale.pizzapartybottomnavbar.BottomNavigationItems
+import edu.farmingdale.pizzapartybottomnavbar.R
 import kotlinx.coroutines.delay
 
 
@@ -40,7 +40,9 @@ fun SplashScreen(navController: NavHostController) {
     }
 
     Column (modifier = Modifier
-        .wrapContentSize(Alignment.Center)){
+        .fillMaxSize()
+        .wrapContentSize(Alignment.Center)
+    ){
         Image(painter = painterResource(id = R.drawable.fsclogo), contentDescription ="" )
     }
 }
